5b113725e8e06a0a627a0e891308d1c50bf58511
60133165 <jamie.gabriel@health.nsw.gov.au>
2021-03-14 17:32:44 +1100

    update

---
+++ Notebooks/.ipynb_checkpoints/Shared_Functions-checkpoint.py
@@ -0,0 +1,10 @@
+# There are some better functions in Sympy and Sage to do this, but we want to keep things as simple as possible for te moment
+def calculate_cross_product(v1, v2):
+    return(v1[0] * v2[1] - v1[1]*v2[0])
+
+
+
+def calculate_signed_area(points, origin_is_one_of_points = True):
+    if origin_is_one_of_points:
+        area = points[1][0] * points[2][1] - points[1][1] * points[2][0]
+    return((area/2).factor())
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Jamie Gabriel
+
+
+
+
+<code>
+	{"shortcuts": 
+    [
+        {
+            "command": "runmenu:restart-and-run-all",
+            "keys": [
+                "Ctrl Shift U"
+            ],
+            "selector": "[data-jp-code-runner]"
+        }
+    ]
+}
+</code>
