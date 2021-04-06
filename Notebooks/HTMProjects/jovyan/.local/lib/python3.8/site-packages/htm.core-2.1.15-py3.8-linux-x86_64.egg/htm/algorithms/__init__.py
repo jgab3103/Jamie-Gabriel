fbdb49aaee307528cf1600bc6ef41410ff4d1e77
jgab3103 <jgab3103@gmail.com>
2021-04-06 11:57:26 +1000

    update to work on mac and windows

--- Notebooks/HTMProjects/jovyan/.local/lib/python3.8/site-packages/htm.core-2.1.15-py3.8-linux-x86_64.egg/htm/algorithms/__init__.py
+++
@@ -1,8 +0,0 @@
-from htm.bindings.algorithms import *
-__all__ = [
-    'SpatialPooler',
-    'TemporalMemory',
-    'Connections',
-    'Classifier',
-    'Predictor',
-]
--- Notebooks/HTMProjects/jovyan/.local/lib/python3.8/site-packages/htm.core-2.1.15-py3.8-linux-x86_64.egg/htm/optimization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Tools for finding good parameters
-
-See htm.optimization.ae
-"""
